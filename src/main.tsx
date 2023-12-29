8e9a019c6074743f42af42f8a88d841ff18df9a0
Nayeemur <nayeem3156@gmail.com>
2023-12-29 12:35:56 -0500

    added profile narrowing algorithm + removed redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import store from "@redux/store";
-import { Provider } from "react-redux";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -14,10 +12,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Provider store={store}>
-          <App />
-          {import.meta.env.VITE_NODE_ENV === "DEV" && <ReactQueryDevtools />}
-        </Provider>
+        <App />
+        {import.meta.env.VITE_NODE_ENV === "DEV" && <ReactQueryDevtools />}
       </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>
